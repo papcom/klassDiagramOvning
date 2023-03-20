2f8da60c57f0e184a1d1cd68b506e8bcb517336f
papcom <92082824+papcom@users.noreply.github.com>
2023-03-20 11:02:59 +0100

    Update Program.cs

--- klassDiagram/klassDiagram/Program.cs
+++ klassDiagram/klassDiagram/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,20 @@ namespace klassDiagram
     {
         static void Main(string[] args)
         {
-
+            string name, email;
+            char gender;
+            Console.Write("enter name: ");
+            name = Console.ReadLine();
+            Console.Clear();
+            Console.Write("enter email: ");
+            email = Console.ReadLine();
+            Console.Clear();
+            Console.Write("enter gender (m/f): ");
+            gender = char.Parse(Console.ReadLine());
+            Console.Clear();
+            Author atur = new Author(name, email, gender);
+            Console.WriteLine(atur.toString());
+            Console.ReadLine();
         }
     }
 }
